30543cabcc070ab04f0b3aa0d56b45ca04471af4
Connor Milliken <cmilliken09@gmail.com>
2018-07-26 08:26:39 -0400

    Added all E-mail functionality

--- app/forms.py
+++ app/forms.py
@@ -30,3 +30,12 @@ class RegistrationForm(FlaskForm):
 class CheckPasswordForm(FlaskForm):
     password = StringField('Password Checker')
     submit = SubmitField('Check')
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
