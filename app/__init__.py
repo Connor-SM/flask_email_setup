d0acdd9f6e97d8dc94dedf71d5599a38bc761f58
Connor Milliken <cmilliken09@gmail.com>
2019-01-14 09:00:10 -0500

    Ready for class

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +13,5 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 login.init_app(app)
-mail = Mail(app)
 
 from app import routes, models
